a9b9042bdc2daa7fae68905ff5c7cc5db0009164
molicancer <molicancer@Molicancers-MacBook-Pro.local>
2014-07-30 09:41:13 +0800

    config

--- lib/dispatcher.rb
+++ lib/dispatcher.rb
@@ -2,10 +2,10 @@ class Dispatcher
 	def self.dispatch message_info
 		return :help if not message_info[:content].nil? and message_info[:content].include? 'help'
 		return :welcome if not message_info[:event].nil? and message_info[:event] == :subscribe
-		return :info if not message_info[:content].nil? and message_info[:content].include? 'info'
-		return :cost if not message_info[:content].nil? and message_info[:content].include? 'cost'
-		return :note if not message_info[:content].nil? and message_info[:content].include? 'note'
-		return :rule if not message_info[:content].nil? and message_info[:content].include? 'rule'
+		return :1 if not message_info[:content].nil? and message_info[:content].include? 'info'
+		return :2 if not message_info[:content].nil? and message_info[:content].include? 'cost'
+		return :3 if not message_info[:content].nil? and message_info[:content].include? 'note'
+		return :4 if not message_info[:content].nil? and message_info[:content].include? 'rule'
 		:picture_message
 	end
 end
--- spec/lib/dispatcher_spec.rb
+++ spec/lib/dispatcher_spec.rb
@@ -4,19 +4,19 @@ describe "#Dispatcher" do
 	end
 
 	it "to info message" do
-		Dispatcher.dispatch({:content => "info"}).should == :info
+		Dispatcher.dispatch({:content => "info"}).should == :1
 	end
 
 	it "to cost message" do
-		Dispatcher.dispatch({:content => "cost"}).should == :cost
+		Dispatcher.dispatch({:content => "cost"}).should == :2
 	end
 
 	it "to note message" do
-		Dispatcher.dispatch({:content => "note"}).should == :note
+		Dispatcher.dispatch({:content => "note"}).should == :3
 	end
 
 	it "to rule message" do
-		Dispatcher.dispatch({:content => "rule"}).should == :rule
+		Dispatcher.dispatch({:content => "rule"}).should == :4
 	end
 
 	it "to help message" do

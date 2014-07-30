4ae58fe6096a23594306a55e893b013a8ad55468
molicancer <molicancer@Molicancers-MacBook-Pro.local>
2014-07-30 09:34:17 +0800

    config

--- spec/lib/dispatcher_spec.rb
+++ spec/lib/dispatcher_spec.rb
@@ -3,6 +3,22 @@ describe "#Dispatcher" do
 		Dispatcher.dispatch({:content => "help"}).should == :help
 	end
 
+	it "to info message" do
+		Dispatcher.dispatch({:content => "info"}).should == :1
+	end
+
+	it "to cost message" do
+		Dispatcher.dispatch({:content => "cost"}).should == :2
+	end
+
+	it "to note message" do
+		Dispatcher.dispatch({:content => "note"}).should == :3
+	end
+
+	it "to rule message" do
+		Dispatcher.dispatch({:content => "rule"}).should == :4
+	end
+
 	it "to help message" do
 		Dispatcher.dispatch({:event => :subscribe}).should == :welcome
 	end
